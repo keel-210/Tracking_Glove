7fb08e16429ac7f636bbea584d12e0074de24414
keel-210 <crafters_touhou@yahoo.co.jp>
2018-05-24 17:28:34 +0900

    BedCode45Finggs

---
+++ Assets/Scripts/FixData25Fings.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+public class FixData25Fings : MonoBehaviour
+{
+    public List<IndexFingerTest> fingerList = new List<IndexFingerTest>();
+    SerialHandler serialHandler;
+    int len;
+    void Start()
+    {
+        serialHandler = GetComponent<SerialHandler>();
+        serialHandler.OnDataReceived += OnDataReceived;
+    }
+    void OnDataReceived(string message)
+    {
+        var _datas = message.Split(' ');
+        for (int i = 0; i < _datas.Length; i++)
+        {
+            fingerList[i].Resistance = int.Parse(_datas[i]);
+        }
+    }
+}
--- Assets/Scripts/IndexFingerTest.cs
+++ Assets/Scripts/IndexFingerTest.cs
@@ -4,10 +4,11 @@ public class IndexFingerTest : MonoBehaviour
 {
     [SerializeField] Transform Index1, Index2, Index3;
     [SerializeField] int maxAngular, minAngular;
+    [SerializeField, Range(0, 1)] float AngleRate1, AngleRate2, AngleRate3;
     public int Resistance;
     Quaternion index1Rot, index2Rot, index3Rot;
     float angle, angularOffset;
-    void Start ()
+    void Start()
     {
         if (Index1)
             index1Rot = Index1.localRotation;
@@ -17,20 +18,20 @@ public class IndexFingerTest : MonoBehaviour
             index3Rot = Index3.localRotation;
         angularOffset = 90f / (maxAngular - minAngular);
     }
-    void Update ()
+    void Update()
     {
-        angle = Mathf.Lerp(angle,(Resistance - minAngular) * angularOffset,0.1f);
+        angle = Mathf.Lerp(angle, (Resistance - minAngular) * angularOffset, 0.1f);
         if (Index1)
         {
-            Index1.localRotation = index1Rot * Quaternion.Euler (angle * Vector3.up);
+            Index1.localRotation = index1Rot * Quaternion.Euler(AngleRate1 * angle * Vector3.up);
         }
         if (Index2)
         {
-            Index2.localRotation = index2Rot * Quaternion.Euler (angle * Vector3.up);
+            Index2.localRotation = index2Rot * Quaternion.Euler(AngleRate2 * angle * Vector3.up);
         }
         if (Index3)
         {
-            Index3.localRotation = index3Rot * Quaternion.Euler (angle * Vector3.up);
+            Index3.localRotation = index3Rot * Quaternion.Euler(AngleRate3 *+ angle * Vector3.up);
         }
     }
 }
